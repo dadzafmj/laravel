c45fc260044a5150b505611edc052f019c249e2c
Augustin <gystmiandrisoa@gmail.com>
2020-03-20 06:50:58 +0300

    second add

---
+++ storage/framework/views/90e708e51f274897031e61d1f075df9d18844295.php
@@ -0,0 +1,26 @@
+<div class="row align-items-center justify-content-xl-between">
+    <div class="col-xl-6">
+        <div class="copyright text-center text-xl-left text-muted">
+            &copy; <?php echo e(now()->year); ?> <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Mention STIC ESP Diego</a> &amp;
+            
+        </div>
+    </div>
+    <div class="col-xl-6">
+        <ul class="nav nav-footer justify-content-center justify-content-xl-end">
+            <li class="nav-item">
+                
+            <li class="nav-item">
+               
+            </li>
+            <li class="nav-item">
+                
+            </li>
+            <li class="nav-item">
+                
+            <li class="nav-item">
+               
+            </li>
+        </ul>
+    </div>
+</div>
+<?php /**PATH E:\projethaja2k20\laravel\resources\views/layouts/footers/nav.blade.php ENDPATH**/ ?>
